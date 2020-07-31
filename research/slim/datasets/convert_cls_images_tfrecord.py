6ee6a84c56fe733f512459ab15156c0d0db177e9
jianzhnie <jianzhnie@126.com>
2020-07-31 19:00:01 +0800

    add data convert tools

---
+++ research/slim/datasets/convert_cls_images_tfrecord.py
@@ -0,0 +1,190 @@
+# Copyright 2016 The TensorFlow Authors. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+r"""Converts Image classification datasets to TFRecords of TF-Example protos.
+
+This module reads the image files data and creates two TFRecord datasets: one for train
+and one for test. Each TFRecord dataset is comprised of a set of TF-Example
+protocol buffers, each of which contain a single image and label.
+
+The script should take about a minute to run.
+
+"""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import math
+import os
+import random
+import sys
+
+from six.moves import range
+from six.moves import zip
+import tensorflow.compat.v1 as tf
+from datasets import dataset_utils
+
+
+
+
+class ImageReader(object):
+	"""Helper class that provides TensorFlow image coding utilities."""
+
+	def __init__(self):
+		# Initializes function that decodes RGB JPEG data.
+		self._decode_jpeg_data = tf.placeholder(dtype=tf.string)
+		self._decode_jpeg = tf.image.decode_jpeg(self._decode_jpeg_data, channels=3)
+
+	def read_image_dims(self, sess, image_data):
+		image = self.decode_jpeg(sess, image_data)
+		return image.shape[0], image.shape[1]
+
+	def decode_jpeg(self, sess, image_data):
+		image = sess.run(self._decode_jpeg,
+						feed_dict={self._decode_jpeg_data: image_data})
+		assert len(image.shape) == 3
+		assert image.shape[2] == 3
+		return image
+
+
+def _get_filenames_and_classes(dataset_root, dataset_name):
+	"""Returns a list of filenames and inferred class names.
+
+	Args:
+		dataset_root: A directory containing a set of subdirectories representing
+		class names. Each subdirectory should contain PNG or JPG encoded images.
+
+	Returns:
+		A list of image file paths, relative to `dataset_root` and the list of
+		subdirectories, representing class names.
+	"""
+	dataset_root = os.path.join(dataset_root, dataset_name)
+	directories = []
+	class_names = []
+	for filename in os.listdir(dataset_root):
+		path = os.path.join(dataset_name, filename)
+		if os.path.isdir(path):
+			directories.append(path)
+		class_names.append(filename)
+
+	photo_filenames = []
+	for directory in directories:
+		for filename in os.listdir(directory):
+			path = os.path.join(directory, filename)
+			photo_filenames.append(path)
+
+	return photo_filenames, sorted(class_names)
+
+
+def _get_dataset_filename(dataset_root, dataset_name, split_name, _NUM_SHARDS, shard_id):
+	output_filename = 'dataset_name_%s_%05d-of-%05d.tfrecord' % (
+		split_name, shard_id, _NUM_SHARDS)
+	return os.path.join(dataset_root, output_filename)
+
+
+def _convert_dataset(dataset_root, dataset_name, split_name, filenames, class_names_to_ids, _NUM_SHARDS):
+	"""Converts the given filenames to a TFRecord dataset.
+
+	Args:
+		split_name: The name of the dataset, either 'train' or 'validation'.
+		filenames: A list of absolute paths to png or jpg images.
+		class_names_to_ids: A dictionary from class names (strings) to ids
+		(integers).
+		dataset_root: The directory where the converted datasets are stored.
+	"""
+	assert split_name in ['train', 'validation']
+
+	num_per_shard = int(math.ceil(len(filenames) / float(_NUM_SHARDS)))
+
+	with tf.Graph().as_default():
+		image_reader = ImageReader()
+
+		with tf.Session('') as sess:
+
+			for shard_id in range(_NUM_SHARDS):
+				output_filename = _get_dataset_filename(
+					dataset_root, dataset_name, split_name, _NUM_SHARDS, shard_id)
+
+				with tf.python_io.TFRecordWriter(output_filename) as tfrecord_writer:
+					start_ndx = shard_id * num_per_shard
+					end_ndx = min((shard_id+1) * num_per_shard, len(filenames))
+					for i in range(start_ndx, end_ndx):
+						sys.stdout.write('\r>> Converting image %d/%d shard %d' % (
+							i+1, len(filenames), shard_id))
+						sys.stdout.flush()
+
+						# Read the filename:
+						image_data = tf.gfile.GFile(filenames[i], 'rb').read()
+						height, width = image_reader.read_image_dims(sess, image_data)
+
+						class_name = os.path.basename(os.path.dirname(filenames[i]))
+						class_id = class_names_to_ids[class_name]
+
+						example = dataset_utils.image_to_tfexample(
+							image_data, b'jpg', height, width, class_id)
+						tfrecord_writer.write(example.SerializeToString())
+
+	sys.stdout.write('\n')
+	sys.stdout.flush()
+
+
+def _dataset_exists(dataset_root, dataset_name, _NUM_SHARDS):
+	for split_name in ['train', 'validation']:
+		for shard_id in range(_NUM_SHARDS):
+			output_filename = _get_dataset_filename(
+			dataset_root, dataset_name, split_name, _NUM_SHARDS, shard_id)
+			if not tf.gfile.Exists(output_filename):
+				return False
+	return True
+
+
+def run(dataset_root, dataset_name,  _NUM_SHARDS, random_seed,val_split=0.2):
+	"""Runs the download and conversion operation.
+
+	Args:
+		dataset_root: The dataset directory where the dataset is stored.
+	"""
+	if not tf.gfile.Exists(dataset_root):
+		tf.gfile.MakeDirs(dataset_root)
+
+	if _dataset_exists(dataset_root, dataset_name, _NUM_SHARDS):
+		print('Dataset files already exist. Exiting without re-creating them.')
+		return
+
+	photo_filenames, class_names = _get_filenames_and_classes(dataset_root, dataset_name)
+	class_names_to_ids = dict(
+		list(zip(class_names, list(range(len(class_names))))))
+
+	# Divide into train and test:
+	random.seed(random_seed)
+	random.shuffle(photo_filenames)
+
+	num_validation = int(len(photo_filenames) * val_split)
+	training_filenames = photo_filenames[num_validation:]
+	validation_filenames = photo_filenames[:num_validation]
+
+	# First, convert the training and validation sets.
+	_convert_dataset(dataset_root, dataset_name,'train', training_filenames, class_names_to_ids,
+					_NUM_SHARDS)
+	_convert_dataset(dataset_root, dataset_name, 'validation', validation_filenames, class_names_to_ids,
+					_NUM_SHARDS)
+
+	# Finally, write the labels file:
+	labels_to_class_names = dict(
+		list(zip(list(range(len(class_names))), class_names)))
+	dataset_utils.write_label_file(labels_to_class_names, dataset_root)
+
+	#_clean_up_temporary_files(dataset_root)
+	print('\nFinished converting the Flowers dataset!')
--- research/slim/eval_image_classifier.py
+++ research/slim/eval_image_classifier.py
@@ -168,8 +168,10 @@ def main(_):
     # Define the metrics:
     names_to_values, names_to_updates = slim.metrics.aggregate_metric_map({
         'Accuracy': slim.metrics.streaming_accuracy(predictions, labels),
-        'Recall_5': slim.metrics.streaming_recall_at_k(
-            logits, labels, 5),
+        'Precision': slim.metrics.streaming_precision(predictions, labels),
+        'Recall'   : slim.metrics.streaming_recall(predictions, labels),
+        'Recall_5': slim.metrics.streaming_recall_at_k(logits, labels, 5),
+        #'Auc'     : slim.metrics.streaming_auc(predictions, labels),
     })
 
     # Print the summaries to screen.
